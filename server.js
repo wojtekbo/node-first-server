f84a6046940be610676aacf6d0f469ee600de9b1
Wojciech Borkowski <wojtek.borkowski@gmail.com>
2022-05-28 00:29:42 +0200

    Add layouts

--- server.js
+++ server.js
@@ -3,41 +3,41 @@ const hbs = require('express-handlebars');
 const path = require('path');
 
 const app = express();
-
 app.engine('.hbs', hbs());
+// app.engine('hbs', hbs({ extname: 'hbs', layoutsDir: './layouts', defaultLayout: 'main' }));
 app.set('view engine', '.hbs');
 
-app.use((req, res, next) => {
-  res.show = name => {
-    res.sendFile(path.join(__dirname, `/views/${name}`));
-  };
-  next();
-});
+// app.use((req, res, next) => {
+//   res.show = name => {
+//     res.sendFile(path.join(__dirname, `/views/${name}`));
+//   };
+//   next();
+// });
 
 app.use(express.static(path.join(__dirname, '/public')));
 
 app.get('/', (req, res) => {
-  res.show('index.html');
+  res.render('index');
 });
 
 app.get('/hello/:name', (req, res) => {
-  res.render('hello', {layout: false, name: req.params.name});
+  res.render('hello', {name: req.params.name});
 });
 
 app.get('/about', (req, res) => {
-  res.show('about.html');
+  res.render('about.hbs', {layout: 'dark'});
 });
 
 app.get('/contact', (req, res) => {
-  res.show('contact.html');
+  res.render('contact');
 });
 
 app.get('/info', (req, res) => {
-  res.show('info.html');
+  res.render('info');
 });
 
 app.get('/history', (req, res) => {
-  res.show('history.html');
+  res.render('history');
 });
 
 app.use((req, res) => {
