aa270cf339f4c3290256570316783949b001979e
Wojciech Borkowski <wojtek.borkowski@gmail.com>
2022-05-29 01:28:40 +0200

    Add contact send message option

--- server.js
+++ server.js
@@ -1,20 +1,19 @@
 const express = require('express');
 const hbs = require('express-handlebars');
+const multer = require('multer');
 const path = require('path');
-
 const app = express();
+
+app.use(express.raw());
+app.use(express.json());
+app.use(express.urlencoded({extended: true}));
+
 app.engine('.hbs', hbs());
 // app.engine('hbs', hbs({ extname: 'hbs', layoutsDir: './layouts', defaultLayout: 'main' }));
 app.set('view engine', '.hbs');
 
-// app.use((req, res, next) => {
-//   res.show = name => {
-//     res.sendFile(path.join(__dirname, `/views/${name}`));
-//   };
-//   next();
-// });
-
 app.use(express.static(path.join(__dirname, '/public')));
+app.use(express.static(path.join(__dirname, '/uploads')));
 
 app.get('/', (req, res) => {
   res.render('index');
@@ -28,8 +27,38 @@ app.get('/about', (req, res) => {
   res.render('about.hbs', {layout: 'dark'});
 });
 
+app.post('/api', (req, res) => {
+  conosle.log(req.body);
+});
+
 app.get('/contact', (req, res) => {
   res.render('contact');
+});
+
+const storage = multer.diskStorage({
+  destination: function (req, file, cb) {
+    cb(null, 'uploads/');
+  },
+  filename: function (req, file, cb) {
+    const uniqueSuffix = (Math.floor(Math.random() * 100000) + 100000).toString().substring(1);
+    cb(null, uniqueSuffix + '-' + file.originalname);
+  },
+});
+
+const upload = multer({storage: storage});
+
+app.post('/contact/send-message', upload.single('fileName'), (req, res) => {
+  const {author, sender, title, message} = req.body;
+  const protocol = req.protocol;
+  const host = req.hostname;
+  const filename = req.file.filename;
+  const fullUrl = `${protocol}://${host}:8000/${filename}`;
+  console.log(fullUrl);
+  if (author && sender && title && message && req.file) {
+    res.render('contact', {isSent: true, fileName: req.file.originalname, filePath: fullUrl});
+  } else {
+    res.render('contact', {isError: true});
+  }
 });
 
 app.get('/info', (req, res) => {
